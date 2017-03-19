67cb5b1d91daf2cf2d7baa531065a498e4217fb7
Arykover <Arykover@DESKTOP-5FSR1B3>
2017-03-20 00:49:15 +0100

    Organisation

--- index.php
+++ index.php
@@ -7,71 +7,71 @@ if(isset($_REQUEST['page'])){
     $page = 'acceuil'; 
     }
 
-        require_once '/vues/v_header.php';
-        require_once '/vues/v_menu.php';
+        require_once dirname(__FILE__).'/vues/v_header.php';
+        require_once dirname(__FILE__).'/vues/v_menu.php';
 
 switch($page){
    case 'acceuil':
-        require_once '/vues/v_acceuil.php';
+        require_once dirname(__FILE__).'/vues/v_acceuil.php';
         break;
     
    case 'BTS' :
-        require_once '/vues/v_BTS.php';
+        require_once dirname(__FILE__).'/vues/v_BTS.php';
         break;
     
     
     case 'PPE1' :
-        require_once '/vues/v_ppe1.php';
+        require_once dirname(__FILE__).'/vues/v_ppe1.php';
         break;
     
     
     case 'PPE2M1' :
-        require_once '/vues/v_ppe2m1.php';
+        require_once dirname(__FILE__).'/vues/v_ppe2m1.php';
         break; 
     
     case 'PPE2M2' :
-        require_once '/vues/v_ppe2m2.php';
+        require_once dirname(__FILE__).'/vues/v_ppe2m2.php';
         break;
    
     
     case 'PPE2M3' :
-        require_once '/vues/v_ppe2m3.php';
+        require_once dirname(__FILE__).'/vues/v_ppe2m3.php';
         break;
     
     
     case 'GLPI' :
-        require_once '/vues/v_GLPI.php';
+        require_once dirname(__FILE__).'/vues/v_GLPI.php';
         break;  
     
     case 'Auto' :
-        require_once '/vues/v_Auto.php';
+        require_once dirname(__FILE__).'/vues/v_Auto.php';
         break;
    
     
     case 'Banque' :
-        require_once '/vues/v_Banque.php';
+        require_once dirname(__FILE__).'/vues/v_Banque.php';
         break;
    
     
     case 'Commercial' :
-        require_once '/vues/v_Commercial.php';
+        require_once dirname(__FILE__).'/vues/v_Commercial.php';
         break;
     
     case 'Contact' :
-        require_once '/vues/v_contact.php';
+        require_once dirname(__FILE__).'/vues/v_contact.php';
         break;
     
     case 'Stage1' :
-        require_once '/vues/v_stage1.php';
+        require_once dirname(__FILE__).'/vues/v_stage1.php';
         break;
     
     case 'Stage2' :
-        require_once '/vues/v_stage2.php';
+        require_once dirname(__FILE__).'/vues/v_stage2.php';
         break;
     }
     
     
-require_once '/vues/v_pied.php';   
+require_once dirname(__FILE__).'/vues/v_pied.php';   
     
 
 ?>
